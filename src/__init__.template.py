1536fa4fe0ec609a3f28489c225ef8f130acacb1
Ferran Velasco <ferranvelasco98@gmail.com>
2018-11-03 17:23:28 +0100

    MAPS_PLACES_URL added

--- src/__init__.template.py
+++ src/__init__.template.py
@@ -3,7 +3,7 @@ HERE_APP_CODE = ''
 HERE_GEO_CODER_URL = 'https://geocoder.api.here.com/6.2/geocode.json'
 
 MAPS_API_KEY = ''
-MAPS_PLACES_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+MAPS_PLACES_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 MAPS_PLACE_DETAILS_URL = 'https://maps.googleapis.com/maps/api/place/details/json'
 
 
