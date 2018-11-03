cb0f6261abe7d19f88477e20bfc3e0268d376011
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-03 16:25:11 +0100

    adding new nlp functions and testing

--- src/nlp/nlp.py
+++ src/nlp/nlp.py
@@ -1,16 +1,26 @@
 from textblob import TextBlob
+from src.dialogflow  import dialogflow
+
 
 def get_sentences(text):
     return text.sentences   
 
 
-""" Given a sentence returns the topic """"
+""" Given a sentence get the entities list"""
 def get_topic(text):
-    # Get nouns 
-    array = []
-    tokens = text.tags
-    for key,value in tokens
-        if value == 'ADJ'
+    
 
 def get_sentiment(text):
     return text.sentiment    
+
+def get_entities(sentences): 
+    # Get entities 
+    return dialogflow.get_entities(str(sentences))
+
+
+def from_text_return_dict(text):
+    text_b = TextBlob(text)
+    sentences = get_sentences(text_b)
+    for sentences in sentences:
+        entities = get_entities(sentences)
+        word_dict = assign_pol(entities)
--- test/test_nlp.py
+++ test/test_nlp.py
@@ -5,10 +5,30 @@ from textblob import TextBlob
 class TestNLP(unittest.TestCase):
 
     def test_divide_sentences(self):
+        text_sample = 'This place is cozy and quiet. But it is not very clean.'
+        text_b = TextBlob(text_sample)
+        sentences = nlp.get_sentences(text_b)
+        self.assertEqual(len(sentences), 2)
 
+    def test_get_adjectives(self):
+        text_sample = 'This place is cozy and quiet'
+        text_b = TextBlob(text_sample)
+        adj = nlp.get_topic(text_b)
+        arr = ['cozy', 'quiet']
+        self.assertEqual(adj,arr)
 
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_get_adjectives_from_sentence(self):
+        text_sample = 'This place is cozy and quiet. But it is not very clean.'
+        text_b = TextBlob(text_sample)
+        adj = nlp.get_topic(text_b)
+        arr = ['cozy', 'quiet','clean']
+        self.assertEqual(adj,arr)
+
+    def get_topic_and_adjective(self):
+        text_sample = 'This place is cozy and quiet'
+        text_b = TextBlob(text_sample)
+    
 
 
 if __name__ == '__main__':
-    unittest.main()import unittest
+    unittest.main()
